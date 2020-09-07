1e2430580d0da0fe2b0ed98bb43e807b3c970e6e
Ghassanee <ghassane20a@gamil.com>
2020-09-07 13:59:48 +0100

    sound and performance

--- src/sample/FlappyBirdV2/Bird.java
+++ src/sample/FlappyBirdV2/Bird.java
@@ -1,125 +1,129 @@
 package sample.FlappyBirdV2;
 
-import javafx.animation.AnimationTimer;
+import javafx.animation.*;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class Bird {
     Image birdd ;
     ImageView birdImg ;
+    ImageView getReady ;
     String birdType ;
     boolean start = false ;
     boolean wait = false ;
     boolean stop = false ;
     AnimationTimer timerBird ;
-    AnimationTimer timerFlying ;
+    Timeline t ;
     Scene scene ;
-    Image birdA1 ;
-    Image birdA2 ;
-    public  Bird(String birdType, Scene scene){
+    Image birdP1;
+    Image birdP2;
+    int UP_GLOBALE = 15;
+
+    public  Bird(String birdType , Scene scene){
         this.birdType = birdType ;
         this.scene = scene ;
-        createBird();
-        birdJumping();
-        birdFlying();
-    }
-    public  void createBird(){
         try {
-            birdImg = new ImageView(new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/"+birdType+"A1.png")));
+           this.birdImg = new ImageView(new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/" +birdType+"1.png")));
+           this.birdd  = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/" +birdType+"0.png"));
+           this.birdP1 = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/" +birdType+"1.png"));
+           this.birdP2 = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/" +birdType+"2.png"));
+           this.getReady = new ImageView(new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/getReady.png")));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        birdImg.setX(412);
-        birdImg.setY(270);
-        birdImg.setFitWidth(50);
-        birdImg.setFitHeight(50);
+        this.birdImg.setX(200);
+        this.birdImg.setY(270);
+        this.birdImg.setScaleX(1.5);
+        this.birdImg.setScaleY(1.5);
+        t = new Timeline(
+                new KeyFrame(Duration.seconds(0), new KeyValue(birdImg.imageProperty() , this.birdP2) /*, new KeyValue(birdImg.yProperty() , birdImg.getY() + 1 )*/),
+                new KeyFrame(Duration.seconds(0.2), new KeyValue(birdImg.imageProperty() , this.birdP1) /*, new KeyValue(birdImg.yProperty() , birdImg.getY() - 1 )*/),
+                new KeyFrame(Duration.seconds(0.4), new KeyValue(birdImg.imageProperty() , this.birdP2) /*, new KeyValue(birdImg.yProperty() , birdImg.getY() - 1 )*/),
+                new KeyFrame(Duration.seconds(0.6), new KeyValue(birdImg.imageProperty() , this.birdP1) /*, new KeyValue(birdImg.yProperty() , birdImg.getY() + 1 )*/)
+
+        );
+        t.setAutoReverse(true);
+        t.setCycleCount(Timeline.INDEFINITE);
+        t.play();
+        getReady.setX(200);
+        getReady.setY(125);
+        getReady.setScaleX(2);
+        getReady.setScaleY(2);
+        birdJumping();
     }
+
     public void birdJumping(){
         try {
-            birdd = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/"+birdType+"D.png"));
+            birdd = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/" +birdType+"1.png"));
+            birdP1 = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/" +birdType+"1.png"));
+            birdP2 = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/" +birdType+"2.png"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        Media sound = new Media(new File("src/sample/FlappyBirdV2/audio/wing.wav").toURI().toString());
+        MediaPlayer wing = new MediaPlayer(sound);
         timerBird = new AnimationTimer() {
-            int j = 0;
-            int up = 12;
+            int i = 0;
+            int up = UP_GLOBALE;
             double down = 0;
             @Override
             public void handle(long l) {
-                if (j%2 == 0 ) {
-                    scene.setOnKeyPressed(keyEvent -> {
-                        if (keyEvent.getCode() == KeyCode.SPACE  && !stop  ) {
-                            start = true;
-                            wait = true;
-                            up = 10 ;
-                            birdImg.setRotate(-20);
-                        }
-                    });
-                    if (wait  && up > -1 ) {
-                        birdImg.setY(birdImg.getY() - up/1.5  );
+                scene.setOnKeyPressed(keyEvent -> {
+                    if (keyEvent.getCode() == KeyCode.SPACE  && !stop  ) {
+                        wing.stop();
+                        wing.play();
+                        start = true;
+                        wait = true;
+                        up = UP_GLOBALE ;
+                        birdImg.setRotate(-20);
+                        getReady.setVisible(false);
+                    }
+                });
+                scene.setOnMouseClicked(event -> {
+                    if (event.getButton() == MouseButton.PRIMARY  && !stop  ) {
+                        start = true;
+                        wait = true;
+                        up = UP_GLOBALE ;
+                        birdImg.setRotate(-20);
+                        getReady.setVisible(false);
 
+                    }
+                });
+                    if (wait  && up > -1 ) {
+                        birdImg.setY(birdImg.getY() - up/2  );
                         up--;
                         if ( up <  0) {
-                            up = 10;
+                            up = UP_GLOBALE;
                             down = 0 ;
                             wait = false;
                         }
                     }
                     if (start && !wait  ) {
-                        if (birdImg.getY() < 520){
+                        if (birdImg.getY() < 560){
                             birdImg.setY(birdImg.getY() + down);
                             if (birdImg.getRotate()<90) {birdImg.setRotate(birdImg.getRotate()+3);}
-                            down = down + 0.6  ;
+                            down = down + 0.5 ;
                         }
                         else {
                             stop = true ;
-                            timerFlying.stop();
                             birdImg.setImage(birdd);
+                            t.stop();
                             timerBird.stop();
                         }
                     }
                 }
-                j++;
-            }
         };
         timerBird.start();
     }
-    public void birdFlying() {
-        try {
-            birdA1 = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/"+birdType+"A1.png"));
-            birdA2 = new Image(new FileInputStream("src/sample/FlappyBirdV2/imgs/"+birdType+"A2.png"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        timerFlying = new AnimationTimer() {
-            int j = 0 ;
-            int i = 0 ;
-            boolean b = false ;
-            @Override
-            public void handle(long l) {
-                if(j%6== 0 ){
-                    if ( i<8 && i>2) {
-                        if (i == 3) {
-                            birdImg.setImage(birdA1);
-                        }
 
-                        birdImg.setY(birdImg.getY() + 1);
-                    }
-                    else {
-                        if (i == 8) birdImg.setImage(birdA2);
-                        birdImg.setY(birdImg.getY() - 1);
-                    }
-                    if (i == 9) i = -1 ;
-                    i++ ;
-                }
-                j++;
-            }
-        };
-        timerFlying.start();
-    }
 }
